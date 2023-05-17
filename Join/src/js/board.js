f744db24dea6cd28cbfce7efb7dd7d26c6325912
Leo Albert <leo.albert.96@web.de>
2023-05-17 13:34:23 +0200

    added add task window to board

---
+++ Join/src/js/board.js
@@ -0,0 +1,7 @@
+function openAddTask(){
+    document.getElementById('add-task-overlay').style.transform = 'translateX(0)';
+}
+
+function closeWindow(){
+    document.getElementById('add-task-overlay').style.transform = 'translateX(2000px)';
+}
